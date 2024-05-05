a61dfe05c5ce2818c0a45ae04464ebe5d8630f11
jaganmohan5525 <jaganmohan5525@gmail.com>
2024-05-05 19:32:40 +0530

    kodeedge2

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "ap-south-1"
+    region = "ap-southeast-2"
   
 }
 
 resource "aws_instance" "ec2demo" {
-    ami = "ami-0fe630eb857a6ec83"
+    ami = "ami-09c8d5d747253fb7a"
     instance_type ="t2.micro"
     tags = {
       Name = "kode-ec2"
